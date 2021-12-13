9dafa54f9c691e1ae126717427300dff2b31e6c1
TobisMa <tobias-ma@gxm.net>
2021-12-13 06:38:04 +0100

    Day13

---
+++ d13/part1.py
@@ -0,0 +1,50 @@
+from typing import List, Set, Tuple
+
+
+def solve(data: List[str]):
+    points: Set[Tuple[int, int]] = {tuple(int(v) for v in x.split(",")) for x in data if x and not x.startswith("fold")}
+    stop = data.index("")
+    foldings = []
+    for line in range(stop + 1, len(data)):
+        folding = data[line].split(" ")[-1]
+        axis, value = folding.split("=")
+        foldings.append((axis, int(value)))
+    
+    for folding in foldings:
+        npoints = fold(folding, points)
+        points = npoints
+        break
+    
+    return len(points)
+        
+def fold(folding, points):
+    # sourcery skip: merge-duplicate-blocks, remove-redundant-if, switch
+    axis, value = folding
+    np = set()
+    for point in points:
+        x, y = point
+        if axis == "x":
+            if x <= value:
+                np.add((x, y))
+            else:
+                d = abs(value - x)
+                np.add((value - d, y))
+
+        elif axis == "y":
+            if y <= value:
+                np.add((x, y))
+            else:
+                d = abs(value - y)
+                np.add((x, value - d))
+            
+    return np
+
+
+def main(filename: str):
+    with open(filename, "r") as f:
+        print(solve(f.read().splitlines()))
+
+
+if __name__ == "__main__":
+    main("d13/input.txt")
+    # main("d13/example.txt")
--- d8/part2.py
+++ d8/part2.py
@@ -21,27 +21,39 @@ def solve(lines):
     total = 0
     for line in lines:
         sl = line.split(" | ")
-        seq = list(map(set, sl[0].split()))
-        out = list(map(set, sl[1].split())) 
-
-        for p in permutations("abcdefg", r=7):
-            mapping = dict(zip(p, "abcdefg"))
-            for sent in seq:
-                digit = "".join(sorted(mapping[x] for x in sent))
-                if digit not in NUMBERS or len(digit) == 7:
+        sent_seq = list(map(set, sl[0].split()))
+        out_seq = list(map(set, sl[1].split())) 
+        
+        for permutation in permutations("abcdefg"):
+            mapping = {l: permutation[i] for i, l in enumerate("abcdefg")}
+            for s in sent_seq:
+                digit = ""
+                for l in s:
+                    digit += mapping[l]
+                
+                digit = ''.join(sorted(list(digit)))
+                
+                if digit not in NUMBERS:
                     break
             else:
                 break
+        else:
+            return "Wtf"
+        
+        number = ""
+        for o in out_seq:
+            d = ""
+            for l in o:
+                d += mapping[l]
+            d = ''.join(sorted(list(d)))
+            n = NUMBERS.index(d)
+            number += str(n)
             
-        shown = ""
-        mapping = {v: k for k, v in mapping.items()}  # type: ignore
-        for res in out:
-            segments = [mapping[d] for d in res]
-            segments.sort()
-            shown += str(NUMBERS.index(''.join(segments)))
-        total += int(shown)
+        total += int(number)
+
 
     return total
+
 
 def main(filename):
     with open(filename, "r") as f:
